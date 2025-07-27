b5c40ca5e8056c747ef55f552f3e6ccd9fd1be8e
asrieljack <1148349418@qq.com>
2025-07-27 16:53:22 +0800

    git push

---
+++ CppPrimerPlus/Chapter3_DataProcessing/Chapter3_ExampleCode/limits.cpp
@@ -0,0 +1,23 @@
+// Program File 3.1
+#include <limits.h>
+
+#include <climits>
+#include <iostream>
+
+int main() {
+  using namespace std;
+  int n_int = INT_MAX;
+  short n_short = SHRT_MAX;
+  long n_long = LONG_MAX;
+  long long n_llong = LONG_LONG_MAX;
+  cout<<"bytes---------";
+  cout << "int: " << sizeof(n_int) << endl;
+  cout << "short: " << sizeof(n_short) << endl;
+  cout << "long: " << sizeof(n_long) << endl;
+  cout << "llong: " << sizeof(n_llong) << endl;
+  cout<<"maximum values------";
+   cout << "int: " << n_int<< endl;
+  cout << "short: " << n_short<< endl;
+  cout << "long: " << n_long << endl;
+  cout << "llong: " << n_llong<< endl;
+}
