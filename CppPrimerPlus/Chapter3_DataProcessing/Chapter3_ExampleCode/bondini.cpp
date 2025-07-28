8b25126c845d50fd53cc3b61560399462bbb9482
asrieljack <1148349418@qq.com>
2025-07-28 11:23:09 +0800

    chapter 3

---
+++ CppPrimerPlus/Chapter3_DataProcessing/Chapter3_ExampleCode/bondini.cpp
@@ -0,0 +1,12 @@
+//Program File 3.7
+ #include<iostream>
+ int main(){
+    using namespace std;
+    cout<<"\aOperation \"HyperHype\" is now activate!\n";
+    cout<<"Enter your agent code: _____\b\b\b\b\b\b\b\b";
+    long code;
+    cin>>code;
+    cout<<"\aYou entered "<<code<<"...\n";
+    cout<<"\aCode verified! Proceed with Plan Z3!\n";
+    return 0;
+ }
---
+++ CppPrimerPlus/Chapter3_DataProcessing/Chapter3_ExampleCode/chartype.cpp
@@ -0,0 +1,10 @@
+//Program File 3.5
+#include <iostream>
+int main(){
+    using namespace std;
+    char ch;
+    cout<<"enter a char: "<<endl;
+    cin>>ch;
+    cout<<"ch: "<<ch;
+    return 0;
+}
