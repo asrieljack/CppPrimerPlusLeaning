e375c3f13c9c03b2766ac2fed68e40acb3f5f928
asrieljack <1148349418@qq.com>
2025-08-04 16:14:39 +0800

    chapter_4exercise

--- CppPrimerPlus/Chapter4_CompoundTypes/Chapter4_Exercises/exercise.cpp
+++ CppPrimerPlus/Chapter4_CompoundTypes/Chapter4_Exercises/exercise.cpp
@@ -1,21 +1,91 @@
 // Program File 4.1
 #include <array>
+#include <cstring>
+#include<vector>
+#include<string>
 #include <iostream>
 using namespace std;
+
 int main() {
-  long obj[50] = {0};  // 系统会把没有被赋值的下标所对应的值都初始化为0
+/*question one----In order to prevent code conflicts,comments were made
+char actor[30];
+short betsie[100];
+float chuck[13];
+long double dipsea[64];
+*/
 
-  double earnings[4]{3.0, 5.6, 7.8, 3.7};  // 可以省略等号
-  unsigned int counts[40] = {};            // all elements set to 0
-  float balances[100]{};                   // all elements set to 0
+//question two
+array<char,30>actor;
+array<short,100>betsie;
+array<float,13>chuck;
+array<long double,64>dipsea;
 
-  // long plifs[]{34,56,34,1.2}
-  // --1.2是不被允许的，禁止不同类型数据在初始化的时候转换 char
-  // slifs[4]{'h','u',303333132,'\o'}--同理，禁止在初始化过程转换缩窄转换
-  char tlifs[4]{'h', 'y', 112, '\0'};  // 这样可以，112没有超出char的取值范围
+//question three
+array<int,5>arr1{1,3,5,7,9};
 
-  for (int i = 0; i <= 49; i++) {
-    cout << obj[i];
-  }
-  return 0;
+//question four
+int even=arr1[0]+arr1[3];
+
+//question five
+array<float,2>ideas{1,2};
+cout<<ideas[1]<<endl;
+
+//question six
+array<char,13>char1{"cheeseburger"};
+
+//question seven
+string str1="Waldorf Salad";
+
+//question eight
+struct Fish{
+  string type;
+  int ounce;
+  double inches;
+};
+
+//question ten
+enum Response{No,Yes,Maybe};
+Response res;
+
+//question eleven;
+double ted=3.0;
+double *ptrTed=&ted;
+cout<<*ptrTed<<endl;
+
+//question twelve
+float treacle[10]{1,2,3,4,5,6,7,8,9,10};
+float *ptrTreacle=treacle;
+cout<<*ptrTreacle<<endl;
+ptrTreacle=(treacle+9);
+cout<<*ptrTreacle;
+
+//question thirteen
+///new
+int num1;
+cout<<"Enter a int number: ";
+cin>>num1;
+int *arr=new int[num1];
+delete [] arr;
+///vector
+vector<int>vec(num1);
+
+//question fourteen
+//cout<<(int*)"Home of the jolly bytes" 会输出首地址的整形整数值，这是因为
+//编译器会把字符串看成const char[N]类型的。
+
+//question fifteen;
+Fish *prtFish=new Fish;
+prtFish->inches=12;
+prtFish->ounce=212;
+prtFish->type="gold fish";
+cout<<"fish's inches: "<<prtFish->inches<<",fish's ounce: "<<prtFish->ounce<<
+",fish's type: "<<prtFish->type;
+
+//question sixteen
+//不知道
+
+//
+const int Size=10;
+vector<string>vecStr(Size);
+array<string,Size>arrStr;
 }
